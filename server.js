c36df76aa1a66ace2c2eb12d1f2caf7668ce977a
Tommy Vinyard <tpvinyard@gmail.com>
2019-10-21 22:15:49 -0500

    adding routes to server and api files

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require("express");
+
+let app = express();
+
+let PORT = provess.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
